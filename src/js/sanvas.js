9d932c75107b378e9909b3bf85749c9476905ad5
Dave Ploeger <ploeger.dave@gmail.com>
2014-09-26 14:34:37 +0200

    canvas scaling and resizing

--- src/js/sanvas.js
+++ src/js/sanvas.js
@@ -1,3 +1,20 @@
 $(function() {
 	
+	// console.log($("canvas").css('width'), $("canvas").css('height'));
+	$("#canvas").attr('width', $( window ).width()-2);
+	$("#canvas").attr('height', $( window ).height()-2);
+	
+	// console.log($("canvas").css('width'), $("canvas").css('height'));
+	$("#canvas").drawArc({
+		draggable: true,
+		fillStyle: "green",
+		x: 100, 
+		y: 100,
+		radius: 50
+	});
+
+	$( window ).resize(function() {
+		$("#canvas").attr('width', $( window ).width()-2);
+		$("#canvas").attr('height', $( window ).height()-2);
+	});
 });
